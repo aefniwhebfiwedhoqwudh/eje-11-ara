db734330c3ab4b49ac10440206ecf67ac15ed4cd
ALEJANDRO <alejandroramirez7981@gmail.com>
2026-05-11 22:05:45 -0600

    sd

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,55 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('EJE-11-ARA');
+
+  nombre: string = '';
+  edad: number | null = null;
+  editandoId: number | null = null;
+
+  mascotas: any[] = [
+    { id: 1, nombre: 'Pluto', edad: 20 },
+    { id: 2, nombre: 'Mickey', edad: 120 },
+    { id: 3, nombre: 'Mark', edad: 20 }
+  ];
+
+  guardar() {
+    if (this.editandoId !== null) {
+     
+      const m = this.mascotas.find(x => x.id === this.editandoId);
+      if (m) {
+        m.nombre = this.nombre;
+        m.edad = this.edad;
+      }
+      this.editandoId = null;
+    } else {
+      this.mascotas.push({
+        id: this.mascotas.length + 1,
+        nombre: this.nombre,
+        edad: this.edad
+      });
+    }
+
+    
+    this.nombre = '';
+    this.edad = null;
+  }
+
+  editar(m: any) {
+    this.nombre = m.nombre;
+    this.edad = m.edad;
+    this.editandoId = m.id;
+  }
+
+  eliminar(id: number) {
+    this.mascotas = this.mascotas.filter(m => m.id !== id);
+  }
 }
